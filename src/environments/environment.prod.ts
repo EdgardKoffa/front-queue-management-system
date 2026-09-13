5e5110f9e917dd6399edf566818d39231003a3ca
Essotina Edgard <edgard@MacBook-Air-de-Essotina.local>
2026-09-13 14:04:46 +0000

    m

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
+const url='https://queue-management-system-4syh.onrender.com'
 export const environment = {
   production: true,
-  apiUrl: 'https://queue-management-system-4syh.onrender.com/api/v1',
-   wsUrl: 'wss://queue-management-system-4syh.onrender.com/ws'
+  apiUrl: url+'/api/v1',
+   wsUrl: url+'/ws'
 };
